cc3aa94afb21d50a071325b6d3ca06c05e8a88e6
Zequi Aguirre <zequi4real@gmail.com>
2022-09-13 21:38:54 -0400

    all files created

--- app.js
+++ app.js
@@ -26,7 +26,12 @@ app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 
 // 👇 Start handling routes here
 const index = require('./routes/index');
+const celebritiesRoute = require('./routes/celebrities.routes.js')
+const moviesRoute = require('./routes/movies.routes.js')
+
 app.use('/', index);
+app.use('/celebrities', celebritiesRoute);
+app.use('/movies', moviesRoute);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
